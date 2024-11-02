028ca21735baa59bb13c2fe09de9944bdeb0abca
kerolos-raouf <83724226+kerolos-raouf@users.noreply.github.com>
2024-11-02 19:23:26 +0200

    finish bottom sheet

--- app/src/main/java/com/filonKiro/fabrico/presentation/cart/ui/CartScreen.kt
+++ app/src/main/java/com/filonKiro/fabrico/presentation/cart/ui/CartScreen.kt
@@ -1,17 +1,28 @@
 package com.filonKiro.fabrico.presentation.cart.ui
 
 import android.widget.Toast
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
@@ -20,9 +31,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -106,21 +121,161 @@ fun CommonBackButton(modifier: Modifier = Modifier) {
     }
 }
 
+@Preview(showSystemUi = true)
 @Composable
 fun CartBottomSheet(
     modifier: Modifier = Modifier,
-    cartViewModel: CartViewModel
+    cartViewModel: CartViewModel = hiltViewModel()
 ) {
 
     Column(
         modifier = modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
     ){
-        TextField(
-            value = cartViewModel.state.couponEditText,
-            onValueChange = {
-                cartViewModel.onEvent(CartEvent.EnterCoupon(it))
-            })
-        Text(text = "Kerolos Raouf", fontFamily = interFontFamily, fontWeight = FontWeight.Bold, fontSize = 20.sp)
+        ApplyEditText(cartViewModel)
+        Spacer(modifier = Modifier.height(30.dp))
+        CustomPriceRow(title = "Sub-Total", price = "$400")
+        Spacer(modifier = Modifier.height(15.dp))
+        CustomPriceRow(title = "Discount", price = "-$20")
+        Spacer(modifier = Modifier.height(15.dp))
+        DashesSeparator()
+        Spacer(modifier = Modifier.height(15.dp))
+        CustomPriceRow(title = "Total Cost", price = "$380")
+        Spacer(modifier = Modifier.height(15.dp))
+        ProceedToCheckoutButton()
+        Spacer(modifier = Modifier.height(30.dp))
     }
 }
+
+@Composable
+fun ProceedToCheckoutButton(
+
+) {
+    val context = LocalContext.current
+    Box(
+        modifier = Modifier.fillMaxWidth().height(50.dp)
+    ){
+        Button(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 20.dp, end = 20.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(context.getColor(R.color.semi_black)),
+                contentColor = Color.White
+            ),
+            shape = RoundedCornerShape(60.dp),
+            onClick = { }
+        ) {
+            Text(
+                text = "Proceed to Checkout",
+                fontSize = 15.sp,
+                fontFamily = interFontFamily
+            )
+        }
+    }
+}
+
+@Composable
+fun DashesSeparator() {
+    Canvas(modifier = Modifier
+        .fillMaxWidth()
+        .padding(start = 20.dp, end = 20.dp)
+        .height(1.dp)) {
+        drawLine(
+            color = Color.Gray,
+            start = Offset(0f, 0f),
+            end = Offset(size.width, 0f),
+            strokeWidth = 3f,
+            pathEffect = PathEffect.dashPathEffect(floatArrayOf(20f, 10f), 0f),
+            cap = StrokeCap.Round
+        )
+    }
+}
+
+@Composable
+fun CustomPriceRow(
+    modifier: Modifier = Modifier,
+    title : String = "Sub-Total",
+    price : String = "$400",
+) {
+    val context = LocalContext.current
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(start = 20.dp, end = 20.dp)
+    ) {
+        Text(
+            modifier = Modifier.weight(1f),
+            text = title,
+            fontFamily = interFontFamily,
+            fontSize = 15.sp,
+            color = Color(context.getColor(R.color.gray))
+        )
+        Text(
+            text = price,
+            fontFamily = interFontFamily,
+            fontSize = 15.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+
+@Composable
+fun ApplyEditText(cartViewModel: CartViewModel = hiltViewModel()) {
+
+    val context = LocalContext.current
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 20.dp, end = 20.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .border(2.dp, Color.LightGray, RoundedCornerShape(50.dp))
+                .height(50.dp)
+                .padding(start = 10.dp, end = 5.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ){
+
+            Box(modifier = Modifier
+                .fillMaxHeight()
+                .weight(1f)
+                .padding(start = 10.dp),
+                contentAlignment = Alignment.CenterStart
+            ){
+                BasicTextField(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 5.dp, bottom = 5.dp),
+                    value = cartViewModel.state.couponEditText,
+                    onValueChange = {
+                        cartViewModel.onEvent(CartEvent.EnterCoupon(it))
+                    },
+                    singleLine = true,
+                    textStyle = TextStyle(color = Color.Black, fontSize = 18.sp),
+                    )
+
+                if(cartViewModel.state.couponEditText.isEmpty()){
+                    Text(text = "Promo Code", fontFamily = interFontFamily,
+                        color = Color(context.getColor(R.color.gray)))
+                }
+            }
+
+
+            Button(
+                onClick = {  },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(context.getColor(R.color.semi_black)),
+                    contentColor = Color.White
+                ),
+                
+            ) {
+                Text(text = "Apply", fontFamily = interFontFamily)
+            }
+        }
+    }
+}
+
